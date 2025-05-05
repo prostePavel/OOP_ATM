5354e3eca75aeb4a44b36b13f6af29ad1b05ab0e
Pavel Dudešek <dudesek.prostepavel@gmail.com>
2025-05-05 15:31:22 +0200

    Added ATM to OOP

--- objects.py
+++ objects.py
@@ -35,6 +35,80 @@ class BankAccount:
             f"({other_account.account_holder} > {self.account_holder}) Transfer accepted. New balance: {self.__balance}")
 
 
+
+class ATM:
+    def menu(self):
+        print("Vítejte v bance!")
+        print("1 - seznam účtů")
+        print("2 - přidat účet")
+        print("3 - převod financí")
+        print("4 - výběr financí")
+        print("5 - vklad financí")
+
+        choice = int(input("Zvolte akci:"))
+        if choice == 1:
+            self.print_accounts()
+        elif choice == 2:
+            self.add_account()
+        elif choice == 3:
+            self.transfer()
+        elif choice == 4:
+            self.withdraw()
+        elif choice == 5:
+            self.deposit()
+        else:
+            print("Chybná akce, vyber znovu")
+
+
+
+    def print_accounts(self):
+        i = 0
+        for bank_account in bank_accounts:
+            print(f"{i} - {bank_account.account_holder}")
+            i += 1
+        self.menu()
+
+    def add_account(self):
+        name = input("Název účtu: ")
+        bank_accounts.append(BankAccount(name))
+
+        self.menu()
+
+    def transfer(self):
+        print("Dostupnýé účty:")
+        self.print_accounts()
+
+        index_from = input("Účet ze kterého:")
+        index_to = input("Účet do kterého:")
+        amount = input("Částka:")
+
+        bank_accounts[int(index_to)].transfer(bank_accounts[int(index_from)], int(amount))
+
+        self.menu()
+
+    def withdraw(self):
+        print("Dostupné účty:")
+        self.print_accounts()
+
+        index_from = input("\nz jakého účtu:")
+        amount = input("částka:")
+
+        bank_accounts[int(index_from)].withdraw(int(amount))
+
+        self.menu()
+
+    def deposit(self):
+        print("Dostupné účty:")
+        self.print_accounts()
+
+        index_to = input("\nDo jakého účtu:")
+        amount = input("částka:")
+
+        bank_accounts[int(index_to)].deposit(int(amount))
+
+        self.menu()
+
+
 #
 #
 #
@@ -42,68 +116,83 @@ class BankAccount:
 #
 bank_accounts = [BankAccount('Jon', 1000)]
 
-def menu():
-    print("Vítejte v bance!")
-    print("1 - seznam účtů")
-    print("2 - přidat účet")
-    print("3 - převod financí")
-    print("4 - výběr financí")
-    print("5 - vklad financí")
+def main():
+    bank = ATM()
 
-    choice = int(input("Zvolte akci:"))
+    choice = int(input("Zvolte 1 pro použití bankomatu:"))
     if choice == 1:
-        print_accounts()
-    elif choice == 2:
-        add_account()
-    elif choice == 3:
-        transfer()
-    elif choice == 4:
-        withdraw()
-    elif choice == 5:
-        deposit()
-    else:
-        print("Chybná akce, vyber znovu")
+        bank.menu()
+        return
 
-    menu()
+main()
 
-def print_accounts():
-    i = 0
-    for bank_account in bank_accounts:
-        print(f"{i} - {bank_account.account_holder}")
-        i += 1
 
-def add_account():
-    name = input("Název účtu: ")
-    bank_accounts.append(BankAccount(name))
 
-def transfer():
-    print("Dostupnýé účty:")
-    print_accounts()
 
-    index_from = input("Účet ze kterého:")
-    index_to = input("Účet do kterého:")
-    amount = input("Částka:")
 
-    bank_accounts[int(index_to)].transfer(bank_accounts[int(index_from)], int(amount))
 
-def withdraw():
-    print("Dostupné účty:")
-    print_accounts()
-
-    index_from = input("\nz jakého účtu:")
-    amount = input("částka:")
-
-    bank_accounts[int(index_from)].withdraw(int(amount))
-
-def deposit():
-    print("Dostupné účty:")
-    print_accounts()
-
-    index_to = input("\nDo jakého účtu:")
-    amount = input("částka:")
-
-    bank_accounts[int(index_to)].deposit(int(amount))
-
-menu()
+#def menu():
+#    print("Vítejte v bance!")
+#    print("1 - seznam účtů")
+#    print("2 - přidat účet")
+#    print("3 - převod financí")
+#    print("4 - výběr financí")
+#    print("5 - vklad financí")
+#
+#    choice = int(input("Zvolte akci:"))
+#    if choice == 1:
+#        print_accounts()
+#    elif choice == 2:
+#        add_account()
+#    elif choice == 3:
+#        transfer()
+#    elif choice == 4:
+#        withdraw()
+#    elif choice == 5:
+#        deposit()
+#    else:
+#        print("Chybná akce, vyber znovu")
+#
+#    menu()
+#
+#def print_accounts():
+#    i = 0
+#    for bank_account in bank_accounts:
+#        print(f"{i} - {bank_account.account_holder}")
+#        i += 1
+#
+#def add_account():
+#    name = input("Název účtu: ")
+#    bank_accounts.append(BankAccount(name))
+#
+#def transfer():
+#    print("Dostupnýé účty:")
+#    print_accounts()
+#
+#    index_from = input("Účet ze kterého:")
+#    index_to = input("Účet do kterého:")
+#    amount = input("Částka:")
+#
+#    bank_accounts[int(index_to)].transfer(bank_accounts[int(index_from)], int(amount))
+#
+#def withdraw():
+#    print("Dostupné účty:")
+#    print_accounts()
+#
+#    index_from = input("\nz jakého účtu:")
+#    amount = input("částka:")
+#
+#    bank_accounts[int(index_from)].withdraw(int(amount))
+#
+#def deposit():
+#    print("Dostupné účty:")
+#    print_accounts()
+#
+#    index_to = input("\nDo jakého účtu:")
+#    amount = input("částka:")
+#
+#    bank_accounts[int(index_to)].deposit(int(amount))
+#
+#menu()
 
 # TODO: dokončit bankomat jako objekt
